3502a77fec7dcdd020ae3b4d391711c09b04b320
Timothy Sharkey <Techne3@gmail.com>
2020-05-04 13:19:58 -0600

    BreakingTheRecord

--- Prep/BreakingTheRecords.js
+++ Prep/BreakingTheRecords.js
@@ -1,13 +1,13 @@
 // https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
 
-function breakingRecords(games) {
-  let most = games[0];
-  let least = games[0];
+function breakingRecords(game) {
+  let most = game[0];
+  let least = game[0];
 
   let dMost = 0;
   let dLeast = 0;
 
-  for (const score of games) {
+  for (const score of game) {
     if (most < score) {
       most = score;
       dMost++;
@@ -22,6 +22,27 @@ function breakingRecords(games) {
 
 console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]));
 
+// function records(game) {
+//   let most = game[0];
+//   let least = game[0];
+
+//   let max = 0;
+//   let min = 0;
+
+//   for (let i =0; i < game.length; i++) {
+//       console.log(game[i])
+//     if (most > game[i]) {
+//       most = game[i];
+//       max++;
+//     } else if (least < game[i]) {
+//       least = game[i];
+//       min++;
+//     }
+//   }
+//   return [max, min];
+// }
+
+// console.log(records([10, 5, 20, 20, 4, 5, 2, 25, 1]));
 function records(game) {
   let most = game[0];
   let least = game[0];
