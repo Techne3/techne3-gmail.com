8ea38cb25be259f492691ab543efc395468f4074
Timothy Sharkey <Techne3@gmail.com>
2020-06-15 13:23:42 -0600

    SherlockSquares HR

---
+++ Prep4/SherlockAndSquares.js
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+
+function squares(a, b) {
+  let start = Math.ceil(Math.sqrt(a));
+  let end = Math.floor(Math.sqrt(b));
+
+  return end - start + 1;
+}
+
+console.log(squares(3, 9));
